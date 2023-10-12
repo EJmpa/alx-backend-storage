097cf891d2951e27542a863e9d6d6a703f9267e6
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-12 12:54:29 +0100

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,14 @@
 -- List bands with 'Glam rock' as their main style, ranked by longevity until 2022
-SELECT band_name, 
-       CASE 
-           WHEN formed > 2022 THEN 0  -- If the band was formed after 2022, set lifespan to 0
-           WHEN split > 2022 THEN 2022 - formed  -- If the band split after 2022, calculate lifespan until 2022
-           ELSE split - formed  -- If neither, calculate the actual lifespan
+
+-- Import the table dump: metal_bands.sql.zip
+-- Column names are band_name and lifespan (in years until 2022)
+-- The script uses attributes formed and split to compute the lifespan
+
+SELECT band_name,
+       CASE
+           WHEN split IS NULL THEN 2022 - formed  -- If the band is still active, calculate lifespan until 2022
+           ELSE split - formed  -- If the band has split, calculate the actual lifespan
        END AS lifespan
 FROM metal_bands
-WHERE band_style = 'Glam rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
