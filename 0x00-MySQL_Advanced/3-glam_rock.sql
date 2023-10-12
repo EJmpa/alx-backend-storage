7e4b4c8875a31a9667b56abc57708db762346d93
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-12 11:44:11 +0000

    MySQL

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- List bands with Glam rock as their main style, ranked by longevity until 2022
+SELECT band_name, 
+       CASE 
+           WHEN formed > 2022 THEN 0  -- If the band was formed after 2022, set lifespan to 0
+           WHEN split > 2022 THEN 2022 - formed  -- If the band split after 2022, calculate lifespan until 2022
+           ELSE split - formed  -- If neither, calculate the actual lifespan
+       END AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
+
