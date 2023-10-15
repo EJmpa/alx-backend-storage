c35543308ffa9559dcaa49f9dfb018c1bdffd5e9
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-15 15:23:26 +0100

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,16 +2,25 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10,2);
+    DECLARE avg_score DECIMAL(10, 2);
 
-    -- Compute the average score
-    SELECT AVG(score) INTO avg_score
+    -- Calculate the average score
+    SELECT AVG(score)
+    INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the average score in the users table
-    UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
-END //
+    -- Update the average score for the user
+    IF avg_score IS NOT NULL THEN  -- Check if there are records for the user
+        UPDATE users
+        SET average_score = avg_score
+        WHERE id = user_id;
+    ELSE
+        -- Handle the case where there are no records for the user
+        UPDATE users
+        SET average_score = 0
+        WHERE id = user_id;
+    END IF;
+END;
+//
 DELIMITER ;
