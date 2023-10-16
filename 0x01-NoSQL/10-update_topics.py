29747f018c0bb833eb6bd8c789e1c0681eaee35b
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-16 11:22:25 +0000

    No-SQL

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Update topics of a school document based on the name.
+"""
+
+from pymongo.collection import Collection
+
+
+def update_topics(mongo_collection: Collection, name: str, topics: list):
+    """
+    Update the topics of a school document in a MongoDB collection.
+
+    Args:
+        mongo_collection (Collection): The pymongo collection object.
+        name (str): The school name to update.
+        topics (list of str): The list of topics to set for the school.
+
+    Returns:
+        None
+    """
+    # Use the update_one method to find the document with the specified name
+    # and update its topics.
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
