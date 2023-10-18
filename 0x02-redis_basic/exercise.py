cc6799cda4549d99f2d867b897724315b8675298
Chukwuemeka Jude Chukwu <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-18 23:11:08 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -34,7 +34,7 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+ 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
