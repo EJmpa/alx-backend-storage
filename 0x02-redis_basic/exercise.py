d03e812f88164d982611ba12e235334e73137978
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-18 22:57:20 +0000

    Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -79,7 +79,7 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
- 
+
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
