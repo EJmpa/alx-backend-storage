d4f456567453b9ca31a88ce8797a5578c78391bb
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-18 21:41:32 +0000

    Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -33,8 +33,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) ->
-    Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None) -> \
+            Union[str, bytes, int, float, None]:
         """
         Retrieve data from Redis using the provided key. Optionally, apply a
         conversion function.
