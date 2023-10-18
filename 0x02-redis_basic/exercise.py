b1d4a20cfffb6c17c394a5f2537cd2b451a5c85f
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-10-18 20:51:53 +0000

    Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Cache module
+"""
+import uuid
+import redis
+
+
+class Cache:
+    """
+    Cache class for storing data in Redis
+    """
+
+    def __init__(self):
+        """
+        Initializes the Cache with a Redis client and flushes the database.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data) -> str:
+        """
+        Store the input data in Redis using a random key and return the key.
+
+        Args:
+            data (Union[str, bytes, int, float]): Data to store.
+
+        Returns:
+            str: The random key used to store the data in Redis.
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
